d8b583daadd237c862505da18adea02b2ecbe799
Elias Silvera <elias.silvera@estudiantes.utec.edu.uy>
2026-05-24 14:30:19 -0300

    dia 1, interpolacion, for, interfaces

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Producto } from './interfaces/producto.interface';
+
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +12,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
+
+
+
 export class App {
   protected readonly title = signal('tienda-app');
+
+  nombre:string="Elias";
+  edad:number=26;
+  productos: Producto[]=[
+    {id:1, nombre:"Laptop", precio:2000, stock:5},
+    {id:2, nombre:"Mouse", precio:900, stock:20},
+    {id:3, nombre:"Teclado", precio:300, stock:10}
+
+  ];
+
+
+  
 }
