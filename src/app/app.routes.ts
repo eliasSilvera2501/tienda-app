28f13ef4253f61380661fdcc7e4b0da7f57f74f2
Elias Silvera <elias.silvera@estudiantes.utec.edu.uy>
2026-05-27 00:15:15 -0300

    dia 6: formularios reactivos, validators

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Inicio } from './components/inicio/inicio';
 import { Detalle } from './components/detalle/detalle';
 import { Tareas } from './components/tareas/tareas';
+import { Formulario } from './components/formulario/formulario';
 
 export const routes: Routes = [
     { path:'', component: Inicio},
     { path:'detalle/:id', component: Detalle},
-    { path:'tareas', component: Tareas}
+    { path:'tareas', component: Tareas},
+    { path:'formulario', component: Formulario}
 
     
 ];
---
+++ src/app/components/formulario/formulario.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [ReactiveFormsModule],
+  templateUrl: './formulario.html',
+  styleUrl: './formulario.scss',
+})
+export class Formulario {
+
+  fb= inject(FormBuilder);
+
+  formulario:FormGroup= this.fb.group({
+
+    nombre:['',Validators.required],
+    email:['', [Validators.required, Validators.email]],
+    password:['', [Validators.required, Validators.minLength(8)]]
+
+
+
+  });
+
+  enviar(){
+    if(this.formulario.valid){
+      console.log(this.formulario.value);
+    }
+  }
+
+}
